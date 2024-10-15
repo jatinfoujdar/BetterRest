b01ed5c4d9b041bba2b592c3af54071ae3b8b695
Jatin Foujdar <jatin321001@gmail.com>
2024-10-15 22:40:52 +0530

    formatted commit

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     var body: some View {
-        Stepper("\(sleepAmount) hours", value : $sleepAmount)
+        Stepper("\(sleepAmount.formatted()) hours", value : $sleepAmount, in: 4...12,step: 0.25)
     }
 }
 
