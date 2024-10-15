9c105abe0939ac26dd84b0cb2747ecc9accce417
Jatin Foujdar <jatin321001@gmail.com>
2024-10-15 20:08:59 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by jatin foujdar on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
