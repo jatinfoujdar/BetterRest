6515054b58336df7f9191a9c9dfd12d8caf82c6f
Jatin Foujdar <jatin321001@gmail.com>
2024-10-16 14:54:25 +0530

    coffee amount commit

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,11 +10,32 @@ import SwiftUI
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value : $sleepAmount, in: 4...12,step: 0.25)
-        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-            .labelsHidden()
+        NavigationStack{
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value : $sleepAmount, in: 4...12,step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper("\(coffeeAmount) cup(s)",value: $coffeeAmount , in: 1...20)
+            }
+        }
     }
+}
+
+func calculatedBedTime(){
+    
 }
 
 #Preview {
