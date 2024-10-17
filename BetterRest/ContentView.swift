c3965c9a64d3d1648861e281068983b2b8d5c0b7
Jatin Foujdar <jatin321001@gmail.com>
2024-10-17 22:41:26 +0530

    connecting swiftui to core ML commit

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -12,6 +12,9 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
     @State private var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     var body: some View {
         NavigationStack{
             VStack {
@@ -35,18 +38,40 @@ struct ContentView: View {
             .toolbar{
                 Button("Calculate", action: calculatedBedtime)
             }
-          }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage) // Show the calculated bedtime here
+            }
+            
         }
-    func calculatedBedtime(){
-        do{
+    }
+    func calculatedBedtime() {
+        do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
             
-        } catch{
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
             
+            let prediction = try model.prediction(wake: Double(hour + minute),
+                                                  estimatedSleep: sleepAmount,
+                                                  coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            showingAlert = true
+            
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem."
+            showingAlert = true 
         }
-     }
     }
+}
 
 
 #Preview {
